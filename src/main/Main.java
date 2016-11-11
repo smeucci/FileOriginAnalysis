d94289a1d3e6a92131cc18ac35adc993d01fa9c3
Saverio <s.meucci91@gmail.com>
2016-11-11 12:54:04 +0100

    updated pom, implemented cmn line options

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,20 +4,105 @@ import testing.Test;
 import training.Train;
 import videoclass.VideoClass;
 
+import org.apache.commons.cli.*;
+
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-				
-		VideoClass classA = new VideoClass("A", "./dataset/classA.txt");
-		VideoClass classB = new VideoClass("B", "./dataset/classB.txt");
+	public static void main(String[] args) throws Exception {
+		CommandLine cmd = parseArguments(args);
+		if (cmd.hasOption("train")) {
+			VideoClass classA = new VideoClass("A", cmd.getOptionValue("listA"));
+			VideoClass classB = new VideoClass("B", cmd.getOptionValue("listB"));
+			new Train(classA, classB, cmd.getOptionValue("output"), true).train();
+		} else if (cmd.hasOption("test")) {
+			new Test(cmd.getOptionValue("input"), cmd.getOptionValue("configA"), cmd.getOptionValue("configB")).test();
+		}
+	}
+
+	private static CommandLine parseArguments(String[] args) {
+			
+		Options opts = new Options();
 		
-		//new Train(classA, classB, "./dataset/", true).train();
+		OptionGroup group = new OptionGroup();
+		group.addOption(new Option("trn", "train", false, "train a binary classification problem"));
+		group.addOption(new Option("tst", "test", false, "predict the class of a xml file"));
+		group.addOption(new Option("h", "help", false, "print help message"));
+		opts.addOptionGroup(group);
 		
-		String video1 = "/media/saverio/DATA/dataset-righini/xmls/galaxys3_dasara/flat/sky_move_1.mp4.xml";
-		String video2 = "/media/saverio/DATA/dataset-righini/xmls/huaweig6_rossana/indoor/garage_move_1.mp4.xml";
-		String video3 = "/media/saverio/DATA/dataset-righini/xmls/ipadmini_marco/indoor/uni_move_1.MOV.xml";
-		new Test(video1, "./dataset/configA-w.xml", "./dataset/configB-w.xml").test();
-	
+		Option listA_opt = Option.builder("lA")
+				.longOpt("listA")
+				.argName("txt file")
+				.hasArg()
+				.desc("text file containing a list of xml file for class A, only for --train")
+				.build();
+		opts.addOption(listA_opt);
+		
+		Option listB = Option.builder("lB")
+				.longOpt("listB")
+				.argName("txt file")
+				.hasArg()
+				.desc("text file containing a list of xml file for class B, only for --train")
+				.build();
+		opts.addOption(listB);
+		
+		Option output_opt = Option.builder("o")
+				.longOpt("output")
+				.argName("folder")
+				.hasArg()
+				.desc("output folder for the training config files, only for --train")
+				.build();
+		opts.addOption(output_opt);
+		
+		Option input_opt = Option.builder("i")
+				.longOpt("input")
+				.argName("xml file")
+				.hasArg()
+				.desc("xml file for which compute the likelihood for class A and B, only for --test")
+				.build();
+		opts.addOption(input_opt);
+		
+		Option configA_opt = Option.builder("cA")
+				.longOpt("configA")
+				.argName("xml file")
+				.hasArg()
+				.desc("xml config file for class A, only for --test")
+				.build();
+		opts.addOption(configA_opt);
+		
+		Option configB_opt = Option.builder("cB")
+				.longOpt("configB")
+				.argName("xml file")
+				.hasArg()
+				.desc("xml config file for class B, only for --test")
+				.build();
+		opts.addOption(configB_opt);
+		
+		CommandLine cl = null;
+		HelpFormatter formatter = new HelpFormatter();
+	    try {
+	        cl = new DefaultParser().parse(opts, args);
+	    } catch (ParseException e) {
+	        System.err.println(e.getMessage());
+	        formatter.printHelp("foa", opts, true);
+	        System.exit(0);
+	    }   
+	    if (cl.hasOption("h")) {
+	        formatter.printHelp("foa", opts, true);
+	        System.exit(0);
+	    }  else if (cl.getOptions().length == 0) {
+	    	System.err.println("No options given");
+	    	formatter.printHelp("foa", opts, true);
+	        System.exit(0);
+	    } else if (cl.hasOption("train") && (!cl.hasOption("listA") ||!cl.hasOption("listB") || !cl.hasOption("o"))) {
+	    	System.err.println("Missing options: [listA | listB | o]");
+	    	formatter.printHelp("foa", opts, true);
+	    	System.exit(0);
+	    } else if (cl.hasOption("test") && (!cl.hasOption("i") ||!cl.hasOption("configA") || !cl.hasOption("configB"))) {
+	    	System.err.println("Missing options: [i | configA | configB]");
+	    	formatter.printHelp("foa", opts, true);
+	    	System.exit(0);
+	    }
+	    return cl;
 	}
 	
 }
