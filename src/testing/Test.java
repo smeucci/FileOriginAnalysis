189e9607b77e9e9ba1076ef255eb08d7ea2a439b
Saverio <s.meucci91@gmail.com>
2016-11-16 15:06:06 +0100

    compute entropy for each node, some refactoring

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -3,7 +3,13 @@ package testing;
 import static com.vftlite.core.VFT.*;
 import static utils.Utils.*;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.log;
 
 import com.vftlite.tree.Field;
 import com.vftlite.tree.Tree;
@@ -34,7 +40,8 @@ public class Test {
 		this.numB = Integer.parseInt(configB.getFieldValue("numOfVideos"));
 		
 		computeLikelihood(tree, configA, configB);
-		System.out.println("Likelihood of " + this.video + " for class A: \n" + likelihood);
+		System.out.println("# Is video '" + this.video + "' of class A?");
+		System.out.println("# Likelihood: " + likelihood + ", Loglikelihood: " + log(likelihood));
 	}
 	
 	protected void computeLikelihood(Tree tree, Tree configA, Tree configB) {
@@ -54,6 +61,8 @@ public class Test {
 	protected void updateLikelihood(Tree node, Tree configA, Tree configB) {
 		//TODO compute entropy
 		if (configA != null && configB != null) {
+			if (verbose) System.out.println("## " + node.getName() + " ##");
+			List<Double> ratios = new ArrayList<Double>();
 			for (Field nodeField: node.getFieldsList()) {
 				String nodeFieldName = nodeField.getName();
 				String nodeFieldValue = nodeField.getValue();
@@ -78,14 +87,21 @@ public class Test {
 						double numerator = getValueWeight(nodeFieldValue, valuesA, weightsA);
 						double denominator = getValueWeight(nodeFieldValue, valuesB, weightsB);
 						likelihood(numerator, denominator);
+						
+						ratios.add(ratio(numerator, denominator));
 					} else {
+						if (verbose) System.out.println("NEW field " + nodeFieldName);
 						likelihood(0, 0);
+						ratios.add(ratio(0, 0));
 					}
 				}
 			}
+			
+			if (verbose) System.out.println("## " + node.getName() + " ENTROPY: " + entropy(ratios) + " ##");
+			
 		} else {
-			if (verbose) System.out.println("NEW: " + node.getName());
-			likelihood(0, 0); //TODO to remove
+			if (verbose) System.out.println("## NEW: " + node.getName() + " ##");
+			likelihood(0, 0);
 		}
 	}
 	
@@ -99,20 +115,48 @@ public class Test {
 	}
 	
 	protected void likelihood(double numerator, double denominator) {
-		//TODO mettere 1
 		if (denominator == 0 && numerator != 0) {
-			if (verbose) System.out.println(likelihood + " - " + (numerator / (1 / (double) this.numB)) + " - " + 1);
-			likelihood = likelihood * (numerator / (1 / (double) this.numB));
+			double ratio = round((numerator / (1 / (double) this.numB)), 3);
+			if (verbose) printDebug(likelihood, ratio, numerator, denominator, 1);
+			likelihood = likelihood * ratio;
 		} else if (numerator == 0 && denominator != 0) {
-			if (verbose) System.out.println(likelihood + " - " + ((1 / (double) this.numA) / denominator) + " - " + 2);
-			likelihood = likelihood * ((1 / (double) this.numA) / denominator);
+			double ratio = round(((1 / (double) this.numA) / denominator), 3);
+			if (verbose) printDebug(likelihood, ratio, numerator, denominator, 2);
+			likelihood = likelihood * ratio;
 		} else if (numerator == 0 && denominator == 0) {
-			if (verbose) System.out.println(likelihood + " - " + (1 / (double) this.numA) + " - " + 34);
-			likelihood = likelihood * (1 / (double) this.numA);
+			double ratio = round((1 / (double) this.numA), 3);
+			if (verbose) printDebug(likelihood, ratio, numerator, denominator, 34);
+			likelihood = likelihood * ratio;
 		} else {
-			if (verbose) System.out.println(likelihood + " - " + (numerator / denominator) + " - " + 0);
-			likelihood = likelihood * (numerator / denominator);
+			double ratio = round((numerator / denominator), 3);
+			if (verbose) printDebug(likelihood, ratio, numerator, denominator, 0);
+			likelihood = likelihood * ratio;
 		}
+	}
+	
+	protected double ratio(double numerator, double denominator) {
+		if (denominator == 0 && numerator != 0) {
+			return round((numerator / (1 / (double) this.numB)), 3);
+		} else if (numerator == 0 && denominator != 0) {
+			return round(((1 / (double) this.numA) / denominator), 3);
+		} else if (numerator == 0 && denominator == 0) {
+			return round((1 / (double) this.numA), 3);
+		} else {
+			return round((numerator / denominator), 3);
+		}
+	}
+	
+	protected double entropy(List<Double> list) {
+		
+		Map<Double, Long> counts =
+			    list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+		
+		double entropy = 0;
+		for (double count: counts.values()) {
+			double x = count / list.size();
+			entropy = entropy - (x * log(x));
+		}
+		return entropy;
 	}
 	
 }
