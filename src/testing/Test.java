a53b26af4781f76727c718b9f8f3ee2b17dbfdba
Saverio <s.meucci91@gmail.com>
2016-11-18 14:54:28 +0100

    refactoring

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import utils.Logger;
 import utils.Pair;
 import static java.lang.Math.log;
 import static java.lang.Math.pow;
@@ -26,138 +27,136 @@ public class Test {
 	private int numB;
 	private boolean verbose;
 	private static double likelihood = 1;
+	private static Logger logger = new Logger();
 	
 	public Test(String url, String config_A, String config_B, boolean verbose) {
 		try {
 			if (url.endsWith("txt")) {
 				this.videos = parseTestFilesList(url);
-				this.verbose = false;
 			} else {
 				this.video = url;
-				this.verbose = verbose;
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println(e.getMessage());
-			System.err.println("The parameter url should be a path to a xml file or a txt file containing a list of xml files to test."
-					+ "(path and class comma separated).");
+			logger.handleException(e);
 		}
 		this.config_A = config_A;
 		this.config_B = config_B;
+		this.verbose = verbose;
+	}
+	
+	protected void run() throws Exception {
+		Tree tree = buildTreeFromXMLFile(this.video);
+		
+		Pair<Tree, Tree> config = new Pair<Tree, Tree>();
+		config.setFirst(buildTreeFromXMLFile(this.config_A));
+		config.setSecond(buildTreeFromXMLFile(this.config_B));
+		
+		this.numA = Integer.parseInt(config.getFirst().getFieldValue("numOfVideos"));
+		this.numB = Integer.parseInt(config.getSecond().getFieldValue("numOfVideos"));
+		
+		computeLikelihood(tree, config);
 	}
 	
 	public void test() throws Exception {
 		run();
-		System.out.println("# Is video '" + this.video + "' of class A?");
-		System.out.println("# Likelihood: " + likelihood + ", Loglikelihood: " + log(likelihood));
+		logger.handleTestResult(this.video, likelihood);
 	}
 	
 	public void batchTest() throws Exception {
-		List<Pair<String, String>> labels = new ArrayList<Pair<String, String>>();
+		List<Pair<String, String>> predictions = new ArrayList<Pair<String, String>>();
 		for (Map.Entry<String, String> entry : this.videos.entrySet()) {
 			this.video = entry.getKey();
 			String label = entry.getValue();
 			run();
 			String predictedLabel = (likelihood > 1) ? "A" : "B";
 
-			System.out.println("# Test video '" + this.video + "':");
-			System.out.println(" - predicted class: " + predictedLabel + "\n"
-							  + " - true class: " + label + "\n"
-							  + " - likelihood: " + likelihood + "\n"
-							  + " - Loglikelihood: " + log(likelihood));
+			logger.handleTestResult(this.video, label, predictedLabel, likelihood);
 			
-			labels.add(new Pair<String, String>(label, predictedLabel));	
+			predictions.add(new Pair<String, String>(label, predictedLabel));	
 			likelihood = 1;
 		}
-		accuracy(labels);
+		accuracy(predictions);
 	}
 	
-	protected void accuracy(List<Pair<String, String>> labels) {
-		double correctlyPredicted = 0;
-		for (Pair<String, String> pair: labels) {
-			if (pair.getKey().equals(pair.getValue())) {
+	protected void accuracy(List<Pair<String, String>> predictions) {
+		int correctlyPredicted = 0;
+		for (Pair<String, String> prediction: predictions) {
+			if (prediction.getFirst().equals(prediction.getSecond())) {
 				correctlyPredicted++;
 			}
 		}
-		System.out.println("Accuracy: " + correctlyPredicted + "/" + labels.size() + ", " 
-							+ round(100 * correctlyPredicted / labels.size(), 3) + " %");
+		logger.handleAccuracy(correctlyPredicted, predictions.size());
 	}
 	
-	protected void run() throws Exception {
-		Tree tree = buildTreeFromXMLFile(this.video);
-		Tree configA = buildTreeFromXMLFile(this.config_A);
-		Tree configB = buildTreeFromXMLFile(this.config_B);
-		
-		this.numA = Integer.parseInt(configA.getFieldValue("numOfVideos"));
-		this.numB = Integer.parseInt(configB.getFieldValue("numOfVideos"));
-		
-		computeLikelihood(tree, configA, configB);
-	}
-	
-	protected void computeLikelihood(Tree tree, Tree configA, Tree configB) {
+	protected void computeLikelihood(Tree tree, Pair<Tree, Tree> config) {
 		if (tree.isLeaf() == false) {
 			Iterator<Tree> treeIterator = tree.iterator();
 			while (treeIterator.hasNext()) {
 				Tree treeChild = treeIterator.next();
-				Tree toCheckA = getCorrespondingChildTree(treeChild, configA);
-				Tree toCheckB = getCorrespondingChildTree(treeChild, configB);
+
+				Pair<Tree, Tree> toCheck = new Pair<Tree, Tree>();
+				toCheck.setFirst(getCorrespondingChildTree(treeChild, config.getFirst()));
+				toCheck.setSecond(getCorrespondingChildTree(treeChild, config.getSecond()));
 				
-				updateLikelihood(treeChild, toCheckA, toCheckB);
-				computeLikelihood(treeChild, toCheckA, toCheckB);
+				updateLikelihood(treeChild, toCheck);
+				computeLikelihood(treeChild, toCheck);
 			}
 		}
 	}
 	
-	protected void updateLikelihood(Tree node, Tree configA, Tree configB) {
-		if (configA != null && configB != null) {
-			if (verbose) System.out.println("## BEGIN " + node.getName().toUpperCase() + " ##");
-			List<Double> ratios = new ArrayList<Double>();
-			for (Field nodeField: node.getFieldsList()) {
-				String nodeFieldName = nodeField.getName();
-				String nodeFieldValue = nodeField.getValue();
-				
-				if (unusedField(nodeFieldName) == false) {
-					if (verbose) System.out.println("- " + nodeFieldName);					
-					Field configFieldA = configA.getFieldByName(nodeFieldName);
-					Field configFieldB = configB.getFieldByName(nodeFieldName);
-					
-					if (configFieldA != null && configFieldB != null) {
-						String configFieldValuesA = configFieldA.getValue();
-						String configFieldValuesB = configFieldB.getValue();
-						
-						String[] splitsA = configFieldValuesA.split("\\;");
-						String[] valuesA = splitsA[0].replaceAll("\\[|\\]", "").split("\\,");
-						double[] weightsA = parseWeights(splitsA[1]);
-						
-						String[] splitsB = configFieldValuesB.split("\\;");
-						String[] valuesB = splitsB[0].replaceAll("\\[|\\]", "").split("\\,");
-						double[] weightsB = parseWeights(splitsB[1]);
-						
-						double numerator = getValueWeight(nodeFieldValue, valuesA, weightsA);
-						double denominator = getValueWeight(nodeFieldValue, valuesB, weightsB);						
-						ratios.add(ratio(numerator, denominator));
-					} else {
-						if (verbose) System.out.println("- NEW " + nodeFieldName);
-						ratios.add(ratio(0, 0));
-					}
-				}
+	protected void updateLikelihood(Tree node, Pair<Tree, Tree> config) {
+		double entropy, factor;
+		if (config.getFirst() != null && config.getSecond() != null) {		
+			if (verbose) logger.handleBeginTag(node.getName());
+			List<Double> ratios = computeRatios(node.getFieldsList(), config);		
+			entropy = entropy(ratios);
+			factor = multiplyRatios(ratios, entropy);
+		} else {	
+			if (verbose) logger.handleNewTag(node.getName());
+			entropy = 0;
+			factor = ratio(0, 0);
+		}
+		likelihood(factor);
+		if (verbose) logger.handleEndTag(node.getName(), entropy, factor, likelihood);
+	}
+	
+	protected List<Double> computeRatios(List<Field> fields, Pair<Tree, Tree> config) {
+		List<Double> ratios = new ArrayList<Double>();
+		for (Field nodeField: fields) {
+			if (unusedField(nodeField.getName()) == false) {
+				double ratio = computeRatio(nodeField.getName(), nodeField.getValue(), config);
+				ratios.add(ratio);
 			}
+		}
+		return ratios;
+	}
+	
+	protected double computeRatio(String nodeFieldName, String nodeFieldValue, Pair<Tree, Tree> config) {		
+		if (verbose) logger.handleField(nodeFieldName);
+		Pair<Field, Field> configFields = new Pair<Field, Field>();
+		configFields.setFirst(config.getFirst().getFieldByName(nodeFieldName));
+		configFields.setSecond(config.getSecond().getFieldByName(nodeFieldName));
+		
+		if (configFields.getFirst() != null && config.getSecond() != null) {
+			String configFieldValuesA = configFields.getFirst().getValue();
+			String configFieldValuesB = configFields.getSecond().getValue();
 			
-			double entropy = entropy(ratios);
-			double factor = multiplyRatios(ratios, entropy);
-			likelihood(factor);
-			if (verbose) System.out.println("## END " + node.getName().toUpperCase() + " - entropy: " + entropy 
-					+ ", factor: " + factor + ", likelihood: " + likelihood + " ##");
+			List<Pair<String, Double>> couplesA = parseValueWeightCouples(configFieldValuesA);
+			List<Pair<String, Double>> couplesB = parseValueWeightCouples(configFieldValuesB);
+			
+			double numerator = getValueWeight(nodeFieldValue, couplesA);
+			double denominator = getValueWeight(nodeFieldValue, couplesB);						
+			return ratio(numerator, denominator);
 		} else {
-			if (verbose) System.out.println("## NEW: " + node.getName() + " ##");
-			likelihood(ratio(0, 0));
+			if (verbose) logger.handleNewField(nodeFieldName);
+			return ratio(0, 0);
 		}
 	}
 	
-	protected double getValueWeight(String value, String[] values, double[] weights) {
-		for (int i = 0; i < values.length; i++) {
-			if (value.equals(values[i])) {
-				return weights[i];
+	protected double getValueWeight(String value, List<Pair<String, Double>> couples) {
+		for (Pair<String, Double> couple: couples) {
+			if (value.equals(couple.getFirst())) {
+				return couple.getSecond();
 			}
 		}
 		return 0;
@@ -179,17 +178,17 @@ public class Test {
 	protected double ratio(double numerator, double denominator) {
 		double ratio;
 		if (denominator == 0 && numerator != 0) {
-			ratio = round((numerator / (1 / (double) this.numB)), 3);
-			if (verbose) printDebug(ratio, numerator, denominator, 1);
+			ratio = round((numerator / (1 / (double) this.numB)), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 1);
 		} else if (numerator == 0 && denominator != 0) {
-			ratio = round(((1 / (double) this.numA) / denominator), 3);
-			if (verbose) printDebug(ratio, numerator, denominator, 2);
+			ratio = round(((1 / (double) this.numA) / denominator), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 2);
 		} else if (numerator == 0 && denominator == 0) {
-			ratio = round((1 / (double) this.numA), 3);
-			if (verbose) printDebug(ratio, numerator, denominator, 34);
+			ratio = round((1 / (double) this.numA), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 34); //TODO 1?
 		} else {
-			ratio = round((numerator / denominator), 3);
-			if (verbose) printDebug(ratio, numerator, denominator, 0);
+			ratio = round((numerator / denominator), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 0);
 		}
 		return ratio;
 	}
