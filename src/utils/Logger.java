3a38bb555bb5ed9589aa1f172f4aac7b46a2e778
Saverio <s.meucci91@gmail.com>
2016-11-18 14:50:38 +0100

    implemented logger class to handle output to console

---
+++ src/utils/Logger.java
@@ -0,0 +1,60 @@
+package utils;
+
+import static java.lang.Math.log;
+import static utils.Utils.round;
+
+public class Logger {
+
+	public void handleException(Exception e) {
+		e.printStackTrace();
+		System.out.println(e.getMessage());
+	}
+	
+	public void handleDebug(double ratio, double numerator, double denominator, int type) {
+		System.out.println("ratio: " + ratio + " - wA: " + numerator + " - wB: " + denominator 
+				+ " - case: " + type);
+	}
+	
+	public void handleSaveTree(String filename) {
+		System.out.println("Saved '" + filename + "'");
+	}
+	
+	public void handleTestResult(String filename, double likelihood) {
+		System.out.println("# Is video '" + filename + "' of class A?");
+		System.out.println("# Likelihood: " + likelihood + ", Loglikelihood: " + log(likelihood));
+	}
+	
+	public void handleTestResult(String filename, String label, String predictedLabel, double likelihood) {
+		System.out.println("# Test video '" + filename + "':");
+		System.out.println(" - predicted class: " + predictedLabel + "\n"
+						  + " - true class: " + label + "\n"
+						  + " - likelihood: " + likelihood + "\n"
+						  + " - Loglikelihood: " + log(likelihood));
+	}
+	
+	public void handleAccuracy(int correctlyPredicted, int tot) {
+		System.out.println("Accuracy: " + correctlyPredicted + "/" + tot+ ", " 
+				+ round(100 * (double) correctlyPredicted / (double) tot, 2) + " %");
+	}
+	
+	public void handleBeginTag(String name) {
+		System.out.println("## BEGIN " + name.toUpperCase() + " ##");
+	}
+	
+	public void handleEndTag(String name, double entropy, double factor, double likelihood) {
+		System.out.println("## END " + name.toUpperCase() + " - entropy: " + entropy 
+				+ ", factor: " + factor + ", likelihood: " + likelihood + " ##");
+	}
+	
+	public void handleField(String name) {
+		System.out.println("- " + name);
+	}
+	
+	public void handleNewField(String name) {
+		System.out.println("- NEW " + name);
+	}
+	
+	public void handleNewTag(String name) {
+		System.out.println("## NEW: " + name.toUpperCase() + " ##");
+	}
+}
