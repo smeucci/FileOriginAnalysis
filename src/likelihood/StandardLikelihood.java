48b561eb4319efac5eb112c940750068734f8794
Saverio <s.meucci91@gmail.com>
2017-02-24 14:39:43 +0100

    implemented StandardLikelihood that implements Likelihood interface

---
+++ src/likelihood/StandardLikelihood.java
@@ -0,0 +1,185 @@
+package likelihood;
+
+import static com.vftlite.core.VFT.getCorrespondingChildTree;
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+import static utils.Utils.parseValueWeightCouples;
+import static utils.Utils.round;
+import static utils.Utils.unusedField;
+import static utils.Utils.unusedTag;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import utils.Logger;
+import utils.Pair;
+
+import com.vftlite.tree.Field;
+import com.vftlite.tree.Tree;
+
+public class StandardLikelihood implements Likelihood {
+	
+	private static double likelihood = 1;
+	private int numA;
+	private int numB;
+	private boolean verbose;
+	private static Logger logger = new Logger();
+	
+	public StandardLikelihood(int numA, int numB, boolean verbose) {
+		this.numA = numA;
+		this.numB = numB;
+		this.verbose = verbose;
+	}
+	
+	public void resetLikelihood() {
+		likelihood = 1;
+	}
+	
+	public double getLikelihood() {
+		return likelihood;
+	}
+	
+	public void computeLikelihood(Tree tree, Pair<Tree, Tree> config) {
+		if (tree.isLeaf() == false) {
+			Iterator<Tree> treeIterator = tree.iterator();
+			while (treeIterator.hasNext()) {
+				Tree treeChild = treeIterator.next();
+
+				if (unusedTag(treeChild) == false) {
+					Pair<Tree, Tree> toCheck = new Pair<Tree, Tree>();
+					toCheck.setFirst(getCorrespondingChildTree(treeChild, config.getFirst()));
+					toCheck.setSecond(getCorrespondingChildTree(treeChild, config.getSecond()));
+					
+					updateLikelihood(treeChild, toCheck);
+					computeLikelihood(treeChild, toCheck);
+				}
+			}
+		}
+	}
+	
+	protected void updateLikelihood(Tree node, Pair<Tree, Tree> config) {
+		double entropy, factor;
+		if (config.isNull() == false) {
+			if (verbose) logger.handleBeginTag(node.getName());
+			List<Double> ratios = computeRatios(node.getFieldsList(), config);		
+			entropy = entropy(ratios);
+			factor = attributesLikelihood(ratios);
+		} else {	
+			if (verbose) logger.handleNewTag(node.getName());
+			entropy = 0;
+			factor = ratio(0, 0);
+		}
+		likelihood = likelihood * factor;
+		if (verbose) logger.handleEndTag(node.getName(), entropy, factor, likelihood);
+	}
+	
+	protected List<Double> computeRatios(List<Field> fields, Pair<Tree, Tree> config) {
+		List<Double> ratios = new ArrayList<Double>();
+		for (Field nodeField: fields) {
+			if (unusedField(nodeField.getName()) == false) {
+				double ratio = computeRatio(nodeField.getName(), nodeField.getValue(), config);
+				ratios.add(ratio);
+			}
+		}
+		return ratios;
+	}
+	
+	protected double computeRatio(String nodeFieldName, String nodeFieldValue, Pair<Tree, Tree> config) {		
+		if (verbose) logger.handleField(nodeFieldName);
+		Pair<Field, Field> configFields = new Pair<Field, Field>();
+		configFields.setFirst(config.getFirst().getFieldByName(nodeFieldName));
+		configFields.setSecond(config.getSecond().getFieldByName(nodeFieldName));
+		
+		if (configFields.isNull() == false) {
+			String configFieldValuesA = configFields.getFirst().getValue();
+			String configFieldValuesB = configFields.getSecond().getValue();
+			
+			List<Pair<String, Double>> couplesA = parseValueWeightCouples(configFieldValuesA);
+			List<Pair<String, Double>> couplesB = parseValueWeightCouples(configFieldValuesB);
+			
+			double numerator = getValueWeight(nodeFieldValue, couplesA);
+			double denominator = getValueWeight(nodeFieldValue, couplesB);						
+			return ratio(numerator, denominator);
+		} else {
+			if (verbose) logger.handleNewField(nodeFieldName);
+			return ratio(0, 0);
+		}
+	}
+	
+	protected double getValueWeight(String value, List<Pair<String, Double>> couples) {
+		for (Pair<String, Double> couple: couples) {
+			if (value.equals(couple.getFirst())) {
+				return couple.getSecond();
+			}
+		}
+		return 0;
+	}
+	
+	protected double attributesLikelihood(List<Double> ratios) {
+		double mult = 1;
+		List<Double> exponents = exponents(ratios);
+		for (int i = 0; i < ratios.size(); i++) {
+			double ratio = ratios.get(i);
+			double exp = exponents.get(i);
+			mult = mult * pow(ratio, exp);
+		}
+		return mult;
+	}
+	
+	protected double ratio(double numerator, double denominator) {
+		double ratio;
+		if (denominator == 0 && numerator != 0) {
+			ratio = round((numerator / (1 / ((double) this.numA + 1))), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 1);
+		} else if (numerator == 0 && denominator != 0) {
+			ratio = round(((1 / ((double) this.numB + 1)) / denominator), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 2);
+		} else if (numerator == 0 && denominator == 0) {
+			ratio = round((1 / ((double) this.numA + 1)), 4); //TODO 1?
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 34);
+		} else {
+			ratio = round((numerator / denominator), 4);
+			if (verbose) logger.handleDebug(ratio, numerator, denominator, 0);
+		}
+		return ratio;
+	}
+	
+	protected double entropy(List<Double> list) {
+		Map<Double, Long> counts =
+			    list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+		double entropy = 0;
+		for (double count: counts.values()) {
+			double x = count / list.size();
+			entropy = entropy - (x * log(x));
+		}
+		return entropy;
+	}
+	
+	protected List<Double> exponents(List<Double> ratios) {
+		List<Double> entropies = entropies(ratios);
+		List<Double> exponents = new ArrayList<Double>();
+		double n = ratios.size();
+		for (double entropy: entropies) {
+			exponents.add(((n - 1)*entropy + 1) / n);
+		}
+		return exponents;
+	}
+	
+	protected List<Double> entropies(List<Double> list) {
+		Map<Double, Long> occurrences =
+			    list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));		
+		List<Double> entropies = new ArrayList<Double>();
+		for (double element: list) {
+			double occurrence = occurrences.get(element);
+			double n = list.size();
+			double x = occurrence / n;
+			double norm = (n == 1) ? 1 : n / log(n);
+			entropies.add(-1*(norm)*(x * log(x)));
+		}
+		return entropies;
+	}
+
+}
--- src/testing/Test.java
+++ src/testing/Test.java
@@ -4,17 +4,14 @@ import static com.vftlite.core.VFT.*;
 import static utils.Utils.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+import likelihood.Likelihood;
+import likelihood.StandardLikelihood;
 import utils.Logger;
 import utils.Pair;
-import static java.lang.Math.log;
-import static java.lang.Math.pow;
 
-import com.vftlite.tree.Field;
 import com.vftlite.tree.Tree;
 
 public class Test {
@@ -26,16 +23,13 @@ public class Test {
 	private int numA;
 	private int numB;
 	private boolean verbose;
-	private static double likelihood = 1;
+	private Likelihood LK;
 	private static Logger logger = new Logger();
 	
 	public Test(String url, String config_A, String config_B, boolean verbose) {
 		try {
-			if (url.endsWith("txt")) {
-				this.videos = parseTestFilesList(url);
-			} else {
-				this.video = url;
-			}
+			if (url.endsWith("txt")) { this.videos = parseTestFilesList(url); } 
+			else { this.video = url; }
 		} catch (Exception e) {
 			logger.handleException(e);
 		}
@@ -53,13 +47,14 @@ public class Test {
 		
 		this.numA = Integer.parseInt(config.getFirst().getFieldValue("numOfVideos"));
 		this.numB = Integer.parseInt(config.getSecond().getFieldValue("numOfVideos"));
+		this.LK = new StandardLikelihood(numA, numB, verbose);
 		
-		computeLikelihood(tree, config);
+		LK.computeLikelihood(tree, config);
 	}
 	
 	public void test() throws Exception {
 		run();
-		logger.handleTestResult(this.video, likelihood);
+		logger.handleTestResult(this.video, LK.getLikelihood());
 	}
 	
 	public void batchTest() throws Exception {
@@ -68,12 +63,12 @@ public class Test {
 			this.video = entry.getKey();
 			String label = entry.getValue();
 			run();
-			String predictedLabel = (likelihood > 1) ? "A" : "B";
+			String predictedLabel = (LK.getLikelihood() > 1) ? "A" : "B";
 
-			logger.handleTestResult(this.video, label, predictedLabel, likelihood);
+			logger.handleTestResult(this.video, label, predictedLabel, LK.getLikelihood());
 			
 			predictions.add(new Pair<String, String>(label, predictedLabel));	
-			likelihood = 1;
+			LK.resetLikelihood();
 		}
 		accuracy(predictions);
 	}
@@ -86,150 +81,6 @@ public class Test {
 			}
 		}
 		logger.handleAccuracy(correctlyPredicted, predictions.size());
-	}
-	
-	protected void computeLikelihood(Tree tree, Pair<Tree, Tree> config) {
-		if (tree.isLeaf() == false) {
-			Iterator<Tree> treeIterator = tree.iterator();
-			while (treeIterator.hasNext()) {
-				Tree treeChild = treeIterator.next();
-
-				if (unusedTag(treeChild) == false) {
-					Pair<Tree, Tree> toCheck = new Pair<Tree, Tree>();
-					toCheck.setFirst(getCorrespondingChildTree(treeChild, config.getFirst()));
-					toCheck.setSecond(getCorrespondingChildTree(treeChild, config.getSecond()));
-					
-					updateLikelihood(treeChild, toCheck);
-					computeLikelihood(treeChild, toCheck);
-				}
-			}
-		}
-	}
-	
-	protected void updateLikelihood(Tree node, Pair<Tree, Tree> config) {
-		double entropy, factor;
-		if (config.isNull() == false) {
-			if (verbose) logger.handleBeginTag(node.getName());
-			List<Double> ratios = computeRatios(node.getFieldsList(), config);		
-			entropy = entropy(ratios);
-			factor = attributesLikelihood(ratios);
-		} else {	
-			if (verbose) logger.handleNewTag(node.getName());
-			entropy = 0;
-			factor = ratio(0, 0);
-		}
-		likelihood(factor);
-		if (verbose) logger.handleEndTag(node.getName(), entropy, factor, likelihood);
-	}
-	
-	protected List<Double> computeRatios(List<Field> fields, Pair<Tree, Tree> config) {
-		List<Double> ratios = new ArrayList<Double>();
-		for (Field nodeField: fields) {
-			if (unusedField(nodeField.getName()) == false) {
-				double ratio = computeRatio(nodeField.getName(), nodeField.getValue(), config);
-				ratios.add(ratio);
-			}
-		}
-		return ratios;
-	}
-	
-	protected double computeRatio(String nodeFieldName, String nodeFieldValue, Pair<Tree, Tree> config) {		
-		if (verbose) logger.handleField(nodeFieldName);
-		Pair<Field, Field> configFields = new Pair<Field, Field>();
-		configFields.setFirst(config.getFirst().getFieldByName(nodeFieldName));
-		configFields.setSecond(config.getSecond().getFieldByName(nodeFieldName));
-		
-		if (configFields.isNull() == false) {
-			String configFieldValuesA = configFields.getFirst().getValue();
-			String configFieldValuesB = configFields.getSecond().getValue();
-			
-			List<Pair<String, Double>> couplesA = parseValueWeightCouples(configFieldValuesA);
-			List<Pair<String, Double>> couplesB = parseValueWeightCouples(configFieldValuesB);
-			
-			double numerator = getValueWeight(nodeFieldValue, couplesA);
-			double denominator = getValueWeight(nodeFieldValue, couplesB);						
-			return ratio(numerator, denominator);
-		} else {
-			if (verbose) logger.handleNewField(nodeFieldName);
-			return ratio(0, 0);
-		}
-	}
-	
-	protected double getValueWeight(String value, List<Pair<String, Double>> couples) {
-		for (Pair<String, Double> couple: couples) {
-			if (value.equals(couple.getFirst())) {
-				return couple.getSecond();
-			}
-		}
-		return 0;
-	}
-	
-	protected double attributesLikelihood(List<Double> ratios) {
-		double mult = 1;
-		List<Double> exponents = exponents(ratios);
-		for (int i = 0; i < ratios.size(); i++) {
-			double ratio = ratios.get(i);
-			double exp = exponents.get(i);
-			mult = mult * pow(ratio, exp);
-		}
-		return mult;
-	}
-	
-	protected void likelihood(double value) {
-		likelihood = likelihood * value;
-	}
-	
-	protected double ratio(double numerator, double denominator) {
-		double ratio;
-		if (denominator == 0 && numerator != 0) {
-			ratio = round((numerator / (1 / ((double) this.numA + 1))), 4);
-			if (verbose) logger.handleDebug(ratio, numerator, denominator, 1);
-		} else if (numerator == 0 && denominator != 0) {
-			ratio = round(((1 / ((double) this.numB + 1)) / denominator), 4);
-			if (verbose) logger.handleDebug(ratio, numerator, denominator, 2);
-		} else if (numerator == 0 && denominator == 0) {
-			ratio = round((1 / ((double) this.numA + 1)), 4); //TODO 1?
-			if (verbose) logger.handleDebug(ratio, numerator, denominator, 34);
-		} else {
-			ratio = round((numerator / denominator), 4);
-			if (verbose) logger.handleDebug(ratio, numerator, denominator, 0);
-		}
-		return ratio;
-	}
-	
-	protected double entropy(List<Double> list) {
-		Map<Double, Long> counts =
-			    list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-		double entropy = 0;
-		for (double count: counts.values()) {
-			double x = count / list.size();
-			entropy = entropy - (x * log(x));
-		}
-		return entropy;
-	}
-	
-	protected List<Double> exponents(List<Double> ratios) {
-		List<Double> entropies = entropies(ratios);
-		List<Double> exponents = new ArrayList<Double>();
-		double n = ratios.size();
-		for (double entropy: entropies) {
-			exponents.add(((n - 1)*entropy + 1) / n);
-		}
-		return exponents;
-	}
-	
-	protected List<Double> entropies(List<Double> list) {
-		Map<Double, Long> occurrences =
-			    list.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));		
-		List<Double> entropies = new ArrayList<Double>();
-		for (double element: list) {
-			double occurrence = occurrences.get(element);
-			double n = list.size();
-			double x = occurrence / n;
-			double norm = (n == 1) ? 1 : n / log(n);
-			entropies.add(-1*(norm)*(x * log(x)));
-		}
-		return entropies;
 	}
 	
 }
