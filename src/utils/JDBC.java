38a7674a98b5f3f79c7d9eb0a6d87c11f30ced5e
Saverio <s.meucci91@gmail.com>
2017-03-08 13:35:54 +0100

    optimized insert with multiple queries

--- src/utils/JDBC.java
+++ src/utils/JDBC.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.sqlite.*;
 
@@ -49,14 +51,14 @@ public class JDBC {
 				+ " id integer PRIMARY KEY AUTOINCREMENT,\n"
 				+ " title text NOT NULL,\n"
 				+ " device_id text NOT NULL,\n"
-				+ " brand text NOT NULL,\n"
-				+ " model text NOT NULL,\n"
-				+ " os text NOT NULL,\n"
-				+ " version text NOT NULL,\n"
+				+ " device_model integer NOT NULL,\n"
+				+ " operating_system integer NOT NULL,\n"
 				+ " pathtofile text NOT NULL,\n"
 				+ " pathtoxml text NOT NULL,\n"
 				+ " pathtoinfo text NOT NULL,\n"
-				+ " UNIQUE(title)\n"
+				+ " UNIQUE(title),\n"
+				+ " FOREIGN KEY(device_model) REFERENCES DeviceModel(id),\n"
+				+ " FOREIGN KEY(operating_system) REFERENCES OperatingSystem(id)\n"
 				+ " )";
 		
 		executeQuery(sqlDeviceModel, URL);
@@ -75,6 +77,25 @@ public class JDBC {
 	    	conn = getConnection(URL);
 	        stmt = conn.createStatement();
 	        stmt.execute(query);
+	        return true;
+	    } catch(SQLException e) {
+	        throw new Exception("Exception during statement execution", e);
+	    } finally {
+	        stmt.close();
+	        conn.close();
+	    }
+	}
+	
+	public static boolean executeMultipleQueries(List<String> queries, String URL) throws Exception {
+		Connection conn = null;
+	    Statement stmt = null;
+	    try {
+	    	conn = getConnection(URL);
+	        stmt = conn.createStatement();
+	        for (String query: queries) {
+	        	stmt.addBatch(query);
+	        }
+	        stmt.executeBatch();
 	        return true;
 	    } catch(SQLException e) {
 	        throw new Exception("Exception during statement execution", e);
@@ -107,6 +128,8 @@ public class JDBC {
 	}
 	
 	private static void update(File folder, String URL, String type) throws Exception {
+		List<String> queries = new ArrayList<String>();
+		queries.add("BEGIN;");
 		File[] files = folder.listFiles();
 		for (File f: files) {
 			if (f.isFile() && !f.getName().matches(".*?\\.txt.*|.*?\\.xml.*") && !f.getName().startsWith(".")) {
@@ -117,17 +140,15 @@ public class JDBC {
 				//parse info xml
 				Info info = new Info(f.getAbsolutePath());
 				//insert to db
-				if (type.equals("training")) {
-					insertVideoTraining(info, URL);
-				} else if (type.equals("testing")) {
-					insertVideoTesting(info, URL);
-				}
+				queries = insertVideo(queries, info, URL, type);
 				System.out.println("updated: " + f.getAbsolutePath());
 			}
 		}
+		queries.add("COMMIT;");
+		executeMultipleQueries(queries, URL);
 	}
 	
-	public static void insertVideoTraining(Info info, String URL) throws Exception {
+	public static List<String> insertVideo(List<String> queries, Info info, String URL, String type) throws Exception {
 		
 		String sqlDeviceModel = "INSERT INTO DeviceModel(id, brand, model)\n"
 				+ " SELECT NULL, '" + info.getManufacturer() + "', '" + info.getModel() + "'\n"
@@ -139,10 +160,14 @@ public class JDBC {
 				+ " WHERE NOT EXISTS(SELECT 1 FROM OperatingSystem\n"
 				+ " WHERE name = '"+ info.getOS() +"' AND version = '" + info.getVersion() + "');";
 		
-		executeQuery(sqlDeviceModel, URL);
-		executeQuery(sqlOperatingSystem, URL);
+		String videoTable = "";
+		if (type.equals("training")) {
+			videoTable = "VideoFile";
+		} else if (type.equals("testing")) {
+			videoTable = "VideoFileTest";
+		}
 		
-		String sqlVideoFile = "INSERT OR IGNORE INTO VideoFile(id, title, device_id, device_model, operating_system, pathtofile, pathtoxml, pathtoinfo)\n"
+		String sqlVideoFile = "INSERT OR IGNORE INTO " + videoTable + "(id, title, device_id, device_model, operating_system, pathtofile, pathtoxml, pathtoinfo)\n"
 				+ " VALUES (NULL,\n"
 				+ " '" + info.getTitle() + "',\n"
 				+ " '" + info.getDeviceID() + "',\n"
@@ -153,26 +178,11 @@ public class JDBC {
 				+ " '" + info.getPathToInfo() + "'\n"
 				+ " );";
 		
-		executeQuery(sqlVideoFile, URL);
+		queries.add(sqlDeviceModel);
+		queries.add(sqlOperatingSystem);
+		queries.add(sqlVideoFile);
 		
-	}
-	
-	public static void insertVideoTesting(Info info, String URL) throws Exception {
-		
-		String sqlVideoFile = "INSERT OR IGNORE INTO VideoFileTest(id, title, device_id, brand, model, os, version, pathtofile, pathtoxml, pathtoinfo)\n"
-				+ " VALUES (NULL,\n"
-				+ " '" + info.getTitle() + "',\n"
-				+ " '" + info.getDeviceID() + "',\n"
-				+ " '" + info.getManufacturer() + "',\n"
-			    + " '" + info.getModel() + "',\n"
-				+ " '" + info.getOS() + "',\n"
-				+ " '" + info.getVersion() + "',\n"
-				+ " '" + info.getPathToFile() + "',\n"
-				+ " '" + info.getPathToXml() + "',\n"
-				+ " '" + info.getPathToInfo() + "'\n"
-				+ " );";
-		
-		executeQuery(sqlVideoFile, URL);
+		return queries;
 		
 	}
 	
